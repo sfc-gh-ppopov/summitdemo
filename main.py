ec51daffc3503384447a88db372fffde383df5d4
Oleksii Bielov <105351625+sfc-gh-obielov@users.noreply.github.com>
2023-06-16 02:14:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from snowflake.snowpark.functions import col
 import logging
 
 
-
+st.title('Power grids report')
+st.text('In this report we will show you the promising areas for building our next')
+st.text('electricity line.')
+st.text("At first let\'s look at the following picture:")
 map_style = eval(open("mapconfig.json").read())
 sess = Session.builder.configs(st.secrets["snowflake"]).create()
 sess.sql("ALTER SESSION SET GEOGRAPHY_OUTPUT_FORMAT='WKT'").collect()
