ea3de58f5c42d044605c949817f1c44ac0ef088a
Oleksii Bielov <105351625+sfc-gh-obielov@users.noreply.github.com>
2023-06-22 10:58:19 +0200

    changed structure

--- main.py
+++ main.py
@@ -8,25 +8,89 @@ import logging
 
 
 st.title('Power Grids Report')
+
+map_style_1 = eval(open("./styles/mapconfig_1.json").read())
+map_style_2 = eval(open("./styles/mapconfig_2.json").read())
+map_style_3 = eval(open("./styles/mapconfig_3.json").read())
+map_style_4 = eval(open("./styles/mapconfig_4.json").read())
+map_style_5 = eval(open("./styles/mapconfig_5.json").read())
+map_style_6 = eval(open("./styles/mapconfig_6.json").read())
+
+sess = Session.builder.configs(st.secrets["snowflake"]).create()
+sess.sql("ALTER SESSION SET GEOGRAPHY_OUTPUT_FORMAT='WKT'").collect()
+
 st.subheader('Summary:')
 st.text('In this report, we aim to identify the most promising areas for the construction of')
 st.text('our upcoming electricity line. At first let\'s look at the following visualization')
 st.text("which depicts areas without mobile network and the access to the electricity.")
-map_style = eval(open("mapconfig.json").read())
-sess = Session.builder.configs(st.secrets["snowflake"]).create()
-sess.sql("ALTER SESSION SET GEOGRAPHY_OUTPUT_FORMAT='WKT'").collect()
-electricity_coverage = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("electricity_coverage"))
-where_to_build = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("where_to_build"))
+
+#=======================================
+st.subheader("Dataset 1: Cell towers")
+st.write("Load information about cell towers from OpenCellId dataset. For each tower we know location, and type of network.")
+cell_towers = sess.table('geolab.demotest.cell_towers_with_coverage').select(col("location"))
+cell_towers = pd.DataFrame(cell_towers.collect())
+map1 = KeplerGl(config = map_style_1)
+map1.add_data(data=cell_towers, name="cell_towers")
+
+keplergl_static(map1, height = 600)
+
+#=======================================
+st.subheader("Dataset 2: Electricity grid")
+st.write("Data about power lines in 5 provinces in the Netherlands. Stored in local coordinate system, SRID=28876")
+
+cables = sess.table('geolab.demotest.nl_cables_stations_viz').select(col("geog"))
+cables = pd.DataFrame(cables.collect())
+
+map2 = KeplerGl(config = map_style_2)
+map2.add_data(data=cables, name="electricity_coverage")
+
+keplergl_static(map2, height = 600)
+
+#=======================================
+st.subheader("Dataset 3: The Netherlands administrative boundaries")
+st.write("Load data from a Shapefile using custom UDF. The coordinate system is UTM, SRID=32231")
 boundaries = sess.table('geolab.demotest.NL_ADMINISTRATIVE_AREAS_viz').select(col("geog"))
-logging.info('CUSTOM LOG: SnowPark DF is there')
-electricity_coverage = pd.DataFrame(electricity_coverage.collect())
-where_to_build = pd.DataFrame(where_to_build.collect())
 boundaries = pd.DataFrame(boundaries.collect())
-logging.info('CUSTOM LOG: Pandas df is there')
 
-map = KeplerGl(config = map_style)
-map.add_data(data=electricity_coverage, name="electricity_coverage")
-map.add_data(data=where_to_build, name="where_to_build")
-map.add_data(data=boundaries, name="boundaries")
+map3 = KeplerGl(config = map_style_3)
+map3.add_data(data=boundaries, name="boundaries")
 
-keplergl_static(map, height = 600, width = 800)
+keplergl_static(map3, height = 600)
+
+
+#=======================================
+st.subheader("Cell towers with coverage polygons")
+st.write("Show polygons of each cell tower")
+
+cell_towers = sess.table('geolab.demotest.cell_towers_with_coverage').select(col("coverage"))
+cell_towers = pd.DataFrame(cell_towers.collect())
+map4 = KeplerGl(config = map_style_4)
+map4.add_data(data=cell_towers, name="cell_towers")
+
+keplergl_static(map4, height = 600)
+
+#=======================================
+st.subheader("Cell coverage per province")
+st.write("Union all coverage polygons")
+
+lte_covered = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("lte_coverage"))
+lte_gaps = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("where_to_build"))
+
+map5 = KeplerGl(config = map_style_5)
+map5.add_data(data=pd.DataFrame(lte_covered.collect()), name="lte_coverage")
+map5.add_data(data=pd.DataFrame(lte_gaps.collect()), name="lte_gaps")
+
+keplergl_static(map5, height = 600)
+
+#=======================================
+st.subheader("Areas where to build new towers")
+st.write("Add power grid data to the picture")
+
+grid_data = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("electricity_coverage_cut"))
+lte_gaps = sess.table('geolab.demotest.grid_lte_coverage_viz').select(col("where_to_build"))
+
+map6 = KeplerGl(config = map_style_6)
+map6.add_data(data=pd.DataFrame(grid_data.collect()), name="grid_data")
+map6.add_data(data=pd.DataFrame(lte_gaps.collect()), name="lte_gaps")
+
+keplergl_static(map6, height = 600)
---
+++ main_local.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import streamlit as st
+from keplergl import KeplerGl
+from streamlit_keplergl import keplergl_static
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col
+import logging
+
+
+st.title('Power Grids Report')
+
+map_style_1 = eval(open("./styles/mapconfig_1.json").read())
+map_style_2 = eval(open("./styles/mapconfig_2.json").read())
+map_style_3 = eval(open("./styles/mapconfig_3.json").read())
+map_style_4 = eval(open("./styles/mapconfig_4.json").read())
+map_style_5 = eval(open("./styles/mapconfig_5.json").read())
+map_style_6 = eval(open("./styles/mapconfig_6.json").read())
+
+sess = Session.builder.configs(st.secrets["snowflake"]).create()
+sess.sql("ALTER SESSION SET GEOGRAPHY_OUTPUT_FORMAT='WKT'").collect()
+
+st.subheader('Summary:')
+st.text('In this report, we aim to identify the most promising areas for the construction of')
+st.text('our upcoming electricity line. At first let\'s look at the following visualization')
+st.text("which depicts areas without mobile network and the access to the electricity.")
+
+#=======================================
+st.subheader("Dataset 1: Cell towers")
+st.write("Load information about cell towers from OpenCellId dataset. For each tower we know location, and type of network.")
+
+df_1 = pd.read_csv('./datasets/dataset_1.csv')
+data_1 = df_1[['LOCATION']]
+
+map1 = KeplerGl(config = map_style_1)
+map1.add_data(data=data_1, name="cell_towers")
+
+keplergl_static(map1, height = 600)
+
+#=======================================
+st.subheader("Dataset 2: Electricity grid")
+st.write("Data about power lines in 5 provinces in the Netherlands. Stored in local coordinate system, SRID=28876")
+
+df_2 = pd.read_csv('./datasets/dataset_2.csv')
+data_2 = df_2[['GEOG']]
+
+map2 = KeplerGl(config = map_style_2)
+map2.add_data(data=data_2, name="electricity_coverage")
+
+keplergl_static(map2, height = 600)
+
+#=======================================
+st.subheader("Dataset 3: The Netherlands administrative boundaries")
+st.write("Load data from a Shapefile using custom UDF. The coordinate system is UTM, SRID=32231")
+
+df_3 = pd.read_csv('./datasets/dataset_3.csv')
+data_3 = df_3[['GEOG']]
+
+map3 = KeplerGl(config = map_style_3)
+map3.add_data(data=data_3, name="boundaries")
+
+keplergl_static(map3, height = 600)
+
+
+#=======================================
+st.subheader("Cell towers with coverage polygons")
+st.write("Show polygons of each cell tower")
+
+df_4 = pd.read_csv('./datasets/dataset_4.csv')
+data_4 = df_4[['COVERAGE']]
+
+map4 = KeplerGl(config = map_style_4)
+map4.add_data(data=data_4, name="cell_towers")
+
+keplergl_static(map4, height = 600)
+
+#=======================================
+st.subheader("Cell coverage per province")
+st.write("Union all coverage polygons")
+
+df_5 = pd.read_csv('./datasets/dataset_5.csv')
+data_5_1 = df_5[['LTE_COVERAGE']]
+data_5_2 = df_5[['WHERE_TO_BUILD']]
+
+map5 = KeplerGl(config = map_style_5)
+map5.add_data(data=data_5_1, name="lte_coverage")
+map5.add_data(data=data_5_2, name="lte_gaps")
+
+keplergl_static(map5, height = 600)
+
+#=======================================
+st.subheader("Areas where to build new towers")
+st.write("Add power grid data to the picture")
+
+df_6 = pd.read_csv('./datasets/dataset_6.csv')
+data_6_1 = df_6[['ELECTRICITY_COVERAGE_CUT']]
+data_6_2 = df_6[['WHERE_TO_BUILD']]
+
+map6 = KeplerGl(config = map_style_6)
+map6.add_data(data=data_6_1, name="grid_data")
+map6.add_data(data=data_6_2, name="lte_gaps")
+
+keplergl_static(map6, height = 600)
